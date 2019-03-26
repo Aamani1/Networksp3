6411b4ca022afd65ca8dcca6bc721d41e29d11a2
Amani Konduru <akonduru2@student.gsu.edu>
2019-03-25 23:48:53 -0400

    testing

---
+++ student-src/page_fault.c
@@ -0,0 +1,87 @@
+#include "paging.h"
+#include "pagesim.h"
+#include "swapops.h"
+#include "stats.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+/*  --------------------------------- PROBLEM 6 --------------------------------------
+    Checkout PDF section 7 for this problem
+
+    Page fault handler.
+
+    When the CPU encounters an invalid address mapping in a page table,
+    it invokes the OS via this handler.
+
+    Your job is to put a mapping in place so that the translation can
+    succeed. You can use free_frame() to make an available frame.
+    Update the page table with the new frame, and don't forget
+    to fill in the frame table.
+
+    When you map a frame, you should update its FIFO timestamp.
+
+    Lastly, you must fill your newly-mapped page with data. If the page
+    has never mapped before, just zero the memory out. Otherwise, the
+    data will have been swapped to the disk when the page was
+    evicted. Call swap_read() to pull the data back in.
+
+    HINTS:
+         - You will need to use the global variable current_process when
+           setting the frame table entry.
+
+    ----------------------------------------------------------------------------------
+ */
+void page_fault(vaddr_t address) {
+    /* First, split the faulting address and locate the page table entry.
+       Remember to keep a pointer to the entry so you can modify it later. */
+
+    vpn_t vpn = vaddr_vpn(address); //get the vpn
+    pte_t* pageTableFaultEntry = (pte_t*) (mem + PTBR * PAGE_SIZE); //the page table of the faulting address
+    pte_t* pageEntry = (pte_t*) (pageTableFaultEntry + vpn); //page number from the page table
+
+    /* It's a page fault, so the entry obviously won't be valid. Grab
+       a frame to use by calling free_frame(). */
+
+    pfn_t freeFrameSlot = free_frame(); //free frame
+
+    /* Update the page table entry. Make sure you set any relevant values. */
+    pageEntry->valid = 1;   // it is in the frame table
+    pageEntry->dirty = 0; // we have not written to it
+    pageEntry->pfn = freeFrameSlot;
+
+    /* Update the frame table. Make sure you set any relevant values. */
+    fte_t* frameEntry = (fte_t*) (frame_table + freeFrameSlot);
+    frameEntry->mapped = 1;
+    frameEntry->process = current_process;
+    frameEntry->vpn = vpn;
+
+    /*
+        Update the timestamp of the appropriate frame table entry with the provided
+        get_current_timestamp function. Timestamps values are used by the FIFO algorithm.
+    */
+    frameEntry->timestamp = get_current_timestamp();   //update time stamp
+
+    /* Initialize the page's memory. On a page fault, it is not enough
+     * just to allocate a new frame. We must load in the old data from
+     * disk into the frame. If there was no old data on disk, then
+     * we need to clear out the memory (why?).
+     *
+     * 1) Get a pointer to the new frame in memory.
+     * 2) If the page has swap set, then we need to load in data from memory
+     *    using swap_read().
+     * 3) Else, just zero the page's memory. If the page is later written
+     *    back, swap_write() will automatically allocate a swap entry.
+     */
+    pte_t* frame_ptr = (pte_t*) (mem + freeFrameSlot * PAGE_SIZE); //pointer to the new frame in memory
+    if (swap_exists(pageEntry)) {   //if the page has swap set
+        swap_read(pageEntry, frame_ptr); //load in data from memory
+    } else {
+        memset(frame_ptr, 0, PAGE_SIZE);    //just zero the pages memory.
+    }
+
+
+
+}
+
+#pragma GCC diagnostic pop
---
+++ student-src/page_replacement.c
@@ -0,0 +1,126 @@
+#include "types.h"
+#include "pagesim.h"
+#include "paging.h"
+#include "swapops.h"
+#include "stats.h"
+#include "util.h"
+
+pfn_t select_victim_frame(void);
+
+/*  --------------------------------- PROBLEM 7 --------------------------------------
+    Checkout PDF section 7 for this problem
+
+    Make a free frame for the system to use.
+
+    You will first call the page replacement algorithm to identify an
+    "available" frame in the system.
+
+    In some cases, the replacement algorithm will return a frame that
+    is in use by another page mapping. In these cases, you must "evict"
+    the frame by using the frame table to find the original mapping and
+    setting it to invalid. If the frame is dirty, write its data to swap!
+ * ----------------------------------------------------------------------------------
+ */
+
+pfn_t free_frame(void) {
+    pfn_t victim_pfn;
+
+    /* Call your function to find a frame to use, either one that is
+       unused or has been selected as a "victim" to take from another
+       mapping. */
+    victim_pfn = select_victim_frame();
+
+    /*
+     * If victim frame is currently mapped, we must evict it:
+     *
+     * 1) Look up the corresponding page table entry
+     * 2) If the entry is dirty, write it to disk with swap_write()
+     * 3) Mark the original page table entry as invalid
+     * 4) Unmap the corresponding frame table entry
+     *
+     */
+
+    if (frame_table[victim_pfn].mapped) {
+        fte_t* frameEntry = (fte_t*) (frame_table + victim_pfn);
+        pte_t* address = (pte_t*) (mem + frameEntry->process->saved_ptbr*PAGE_SIZE);
+        pte_t* pageEntry = (pte_t*) (address + frame_table[victim_pfn].vpn);
+
+        if (pageEntry->dirty) {
+            
+            stats.writebacks ++;  //increment write backs because we are writing to disk (I/O queue)
+            swap_write(pageEntry, mem + ((victim_pfn) * PAGE_SIZE));
+            pageEntry->dirty = 0; //page is not dirty anymore
+        }
+        pageEntry->valid = 0; //not in the frame anymore
+        frame_table[victim_pfn].mapped = 0; //not in use
+    }
+
+    /* Return the pfn */
+    return victim_pfn;
+}
+
+
+
+/*  --------------------------------- PROBLEM 9 --------------------------------------
+    Checkout PDF section 7 and 9 for this problem
+
+    Finds a free physical frame. If none are available, uses either a
+    randomized or FIFO algorithm to find a used frame for
+    eviction.
+
+    Return:
+        The physical frame number of a free (or evictable) frame.
+
+    HINTS: Use the global variables MEM_SIZE and PAGE_SIZE to calculate
+    the number of entries in the frame table.
+    ----------------------------------------------------------------------------------
+*/
+pfn_t select_victim_frame() {
+    /* See if there are any free frames first */
+    size_t num_entries = MEM_SIZE / PAGE_SIZE;
+    for (size_t i = 0; i < num_entries; i++) {
+        if (!frame_table[i].protected && !frame_table[i].mapped) {
+            return i;
+        }
+    }
+
+    if (replacement == RANDOM) {
+        /* Play Russian Roulette to decide which frame to evict */
+        pfn_t last_unprotected = NUM_FRAMES;
+        for (pfn_t i = 0; i < num_entries; i++) {
+            if (!frame_table[i].protected) {
+                last_unprotected = i;
+                if (prng_rand() % 2) {
+                    return i;
+                }
+            }
+        }
+        /* If no victim found yet take the last unprotected frame
+           seen */
+        if (last_unprotected < NUM_FRAMES) {
+            return last_unprotected;
+        }
+    } else if (replacement == FIFO) {
+        /* Implement a FIFO algorithm here */
+        timestamp_t longest = get_current_timestamp();
+        pfn_t first = 0;
+        for (pfn_t i = 0; i < num_entries; i++) { //iterate over the all the entries
+            if (!frame_table[i].protected) {    //if not protected
+                if (frame_table[i].timestamp < longest) {   //if less than longest
+                    first = i;
+                    longest = frame_table[i].timestamp; //update the time
+                }
+            }
+        }
+        return first;    
+
+    } else if (replacement == CLOCKSWEEP) {
+        /* Optionally, implement the clocksweep algorithm here */
+     
+    }
+
+    /* If every frame is protected, give up. This should never happen
+       on the traces we provide you. */
+    panic("System ran out of memory\n");
+    exit(1);
+}
